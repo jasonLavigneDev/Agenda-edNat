abae5bd1fc4f8796184570eb0468080235319b65
Francois AUBEUT <contact@mexar.fr>
2021-06-21 11:36:39 +0200

    fix linter

--- app/tests/groups.tests.js
+++ app/tests/groups.tests.js
@@ -18,13 +18,12 @@ describe('groups', function groupTests() {
 
   describe('publications', function groupPublications() {
     let currentUser;
-    let anotherUser;
     let Ids;
     beforeEach(function beforeTesting() {
       Groups.remove({});
       Meteor.users.remove({});
-      anotherUser = changeCurrentUser();
       currentUser = changeCurrentUser();
+      changeCurrentUser();
       Ids = [Random.id(), Random.id(), Random.id(), Random.id(), Random.id()];
       Ids.forEach((_id) => {
         Factory.create('group', {
@@ -32,13 +31,14 @@ describe('groups', function groupTests() {
         });
       });
       Factory.create('group', {
-        members: [anotherUser._id],
+        members: [currentUser._id],
       });
+      changeCurrentUser(currentUser);
     });
 
     describe('groups.member', function groupsForUserPub() {
       it('sends all groups for the currentUser', function groupsMembersPubTest(done) {
-        const collector = new PublicationCollector({ userId: anotherUser._id });
+        const collector = new PublicationCollector({ userId: currentUser._id });
         collector.collect('groups.member', {}, (collections) => {
           assert.equal(collections.groups.length, 1);
           done();
@@ -48,7 +48,7 @@ describe('groups', function groupTests() {
     describe('groups.list', function groupsForUserPub() {
       it('sends all groups from a list of ids', function groupsListPubTest(done) {
         const groupsIds = [Ids[0], Ids[2], Ids[4]];
-        const collector = new PublicationCollector({ userId: anotherUser._id });
+        const collector = new PublicationCollector({ userId: currentUser._id });
         collector.collect('groups.list', { groupsIds }, (collections) => {
           assert.equal(collections.groups.length, 3);
           done();
