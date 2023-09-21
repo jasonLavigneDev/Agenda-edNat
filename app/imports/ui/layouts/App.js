27fa585874eedb9bb7eb7c6ae9fbafd26cca321e
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2023-09-21 12:28:11 +0200

    feat(matomo): integrate matomo client

--- app/imports/ui/layouts/App.js
+++ app/imports/ui/layouts/App.js
@@ -1,12 +1,14 @@
 import React, { Suspense, lazy, useState, useEffect } from 'react';
 import { Accounts } from 'meteor/accounts-base';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { MatomoProvider, useMatomo } from '@datapunt/matomo-tracker-react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Spinner from '../components/system/Spinner';
 import MsgHandler from '../components/system/MsgHandler';
 import DynamicStore from '../contexts/context';
 import lightTheme from '../themes/light';
+import instance from '../utils/matomo';
 
 // dynamic imports
 const MainLayout = lazy(() => import('./MainLayout'));
@@ -18,8 +20,11 @@ function App() {
       setUserFailed(true);
     }
   });
+  const { trackPageView, enableLinkTracking } = useMatomo();
+  enableLinkTracking();
 
   useEffect(() => {
+    trackPageView();
     return () => {
       if (typeof stopCallback.stop === 'function') {
         stopCallback.stop();
@@ -43,11 +48,13 @@ function App() {
 }
 
 export default () => (
-  <MuiThemeProvider theme={lightTheme}>
-    <BrowserRouter>
-      <DynamicStore>
-        <App />
-      </DynamicStore>
-    </BrowserRouter>
-  </MuiThemeProvider>
+  <MatomoProvider value={instance}>
+    <MuiThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <DynamicStore>
+          <App />
+        </DynamicStore>
+      </BrowserRouter>
+    </MuiThemeProvider>
+  </MatomoProvider>
 );
